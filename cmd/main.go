c2666ed5831d024513e0920e47fb500cab33c9df
Jetta Jacks <jettajac@su-d3.msk.21-school.ru>
2024-06-12 14:19:46 +0300

    06.12_14:19_first

---
+++ cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+// protoc -I proto --go_out=plugins=grpc:cmd proto/sso/sso.proto
+// protoc -I proto proto/sso/sso.proto --go_out=./gen/go --go_opt=paths=source_relative --go-grpc_out=./gen/go/ --go-grpc_opt=paths=source_relative
+// go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+// go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Start app")
+}
