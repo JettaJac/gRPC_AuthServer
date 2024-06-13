5dfb5b39436259b369f40d3f1ba26d652740db6a
Jetta Jacks <jettajac@su-d3.msk.21-school.ru>
2024-06-13 15:10:43 +0300

    06.13_15:10_Прописаны логгер

--- sso/cmd/sso/main.go
+++ sso/cmd/sso/main.go
@@ -8,12 +8,37 @@ package main
 import (
 	"app/internal/config"
 	"fmt"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	fmt.Println("Start app")
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+	log.Info("starting application", slog.String("env", cfg.Env))
 }
 
-func setupLogger(env string)
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
+}
